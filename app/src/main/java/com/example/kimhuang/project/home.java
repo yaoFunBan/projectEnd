3136d068815af72494812636c8c15abe2797ec21
YaoFunBan <wucharat.th@gmail.com>
2016-04-05 13:49:38 +0700

    add class dialog in home.java and call show dialog but can't onclicklistener inbutton on dialog_setting

--- app/src/main/java/com/example/kimhuang/project/home.java
+++ app/src/main/java/com/example/kimhuang/project/home.java
@@ -1,6 +1,7 @@
 package com.example.kimhuang.project;
 
 import android.annotation.TargetApi;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -8,8 +9,11 @@ import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class home extends AppCompatActivity implements View.OnClickListener {
     Button btn_play1, btn_setting1, btn_con1, btn_close;
@@ -52,8 +56,7 @@ public class home extends AppCompatActivity implements View.OnClickListener {
                 startActivity(i);
                 break;
             case (R.id.btn_setting1):
-                i = new Intent(this, scene1_1.class);
-                startActivity(i);
+                displayDiaglogSetting();
                 break;
             case (R.id.btn_con1):
                 i = new Intent(this, contact.class);
@@ -84,6 +87,19 @@ public class home extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+    }
+
+    public void displayDiaglogSetting() {
+        Dialog dsetting = new Dialog(this, android.R.style.Theme_Translucent_NoTitleBar);
+        dsetting.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dsetting.setContentView(R.layout.setting_dialog);
+
+        Window window = dsetting.getWindow();
+        window.setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        dsetting.show();
+
 
     }
 }

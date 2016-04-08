4d7fa917c39fc72fd5929c2b91e6bae0fff10873
Kimhuang <pronkamon10@gmail.com>
2016-04-08 11:25:32 +0700

    commit change layout game1

---
+++ app/src/main/java/com/example/kimhuang/project/game1.java
@@ -0,0 +1,14 @@
+package com.example.kimhuang.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class game1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game1);
+
+    }
+}

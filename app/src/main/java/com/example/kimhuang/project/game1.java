b6858780bb784484785015ce73934fde7258ba6f
Kimhuang <pronkamon10@gmail.com>
2016-04-21 11:51:03 +0700

    game

--- app/src/main/java/com/example/kimhuang/project/game1.java
+++ app/src/main/java/com/example/kimhuang/project/game1.java
@@ -3,14 +3,18 @@ package com.example.kimhuang.project;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class game1 extends AppCompatActivity {
     CountDownTimer cdt;
+    TextView tvTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game1);
+
+        tvTimer = (TextView)findViewById(R.id.tvTimer);
 
 
         CountDownTimer cdt = new CountDownTimer(10000, 1000) {
